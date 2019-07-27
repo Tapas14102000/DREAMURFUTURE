932c5ecad4b847f2e74a424e48f37abe50e4f2f0
Tapas14102000 <53347922+Tapas14102000@users.noreply.github.com>
2019-07-27 14:57:58 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class q1 {
+
+	public static void main(String[] args) 
+	{
+		  int s=0;
+		     for(int i=0;i<1000;i++)
+		     {
+		          if(i%3==0||i%5==0)
+		               s+=i;
+		     }
+		     System.out.println("RESULT="+s);
+
+	}
+
+}
