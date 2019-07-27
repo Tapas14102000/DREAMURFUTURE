9e33a8e4ac1d416081fe30f1562d8190f0b2efc4
Tapas14102000 <53347922+Tapas14102000@users.noreply.github.com>
2019-07-27 22:11:11 +0530

    Update q5.java

--- q5.java
+++ q5.java
@@ -1,30 +1,27 @@
 package hello;
 
-public class q5 {
+public class q5
+{
 
 	public static void main(String[] args) 
 	{
-		 int a=1,k=1;
-	     while(a!=0)
-	     {
-	          int flag=0;
-	          for(int i=1;i<=20;i++)
-	          {
+		 int k=1,flag=0;
+		 while(flag==0)
+		 {
+	            for(int i=2;i<=20;i++)
+	             {
 	               if(k%i!=0)
-	                    break;
-	                    else if(i==20)
+	                    {k++;break;}
+	               else if(i==20)
 	                    {
-	                         flag=1;
+	            	   	 flag=1;
+	                    	 System.out.println(k+" is the smallest positive no. that is evenly divisible by all of the numbers from 1 t0 20.");  
 	                         break;
 	                    }
-	          }
-	          k++;
-	          if (flag==1)
-	          {
-	               a=0;
-	               System.out.println((k-1)+" is the smallest positive no. that is evenly divisible by all of the numbers from 1 t0 20.");
-	          }
-	     }
+	             }
+		 }    
+	   
+	     
 	}
 
 }
