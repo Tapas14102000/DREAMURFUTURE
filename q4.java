185acb120d59c829961b877834be56be20effbec
Tapas14102000 <53347922+Tapas14102000@users.noreply.github.com>
2019-07-28 12:37:08 +0530

    Update q4.java

--- q4.java
+++ q4.java
@@ -4,35 +4,22 @@ public class q4 {
 
 	public static void main(String[] args)
 	{
-		 int max=-1,i,j=0;
+		 int max=-1,i,j;
 	     for( i=100;i<1000;i++)
 	     {
 	          for( j=100;j<1000;j++)
 	          {
 	               int k=i*j;
-	               int ans=ispalindrome(k);
-	               if(ans==0&&k>max)
+	               String ans=String.valueOf(k);
+	               StringBuffer rev=new StringBuffer(ans);
+	               rev.reverse();
+	               if(ans.contentEquals(rev)&&k>max)
 	               {max=k;}
 	          }
 	               
 	     }
 	     System.out.println(max+" is the largest palindrome number.");
 
-
-	}
-	static int ispalindrome(int a)
-	{
-	     int k=a,s=0;
-	     while(a!=0)
-	     {
-	          int r=a%10;
-	          s=s*10+r;
-	          a/=10;
-	     }
-	     if(k==s)
-	          return 0;
-	     else
-	          return 1;
 
 	}
 
