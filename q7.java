c54c014ef9a531fc8b56e324ca6d1f6ddf80c6bd
Tapas14102000 <53347922+Tapas14102000@users.noreply.github.com>
2019-07-30 16:10:37 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -4,23 +4,25 @@ public class q7 {
 
 	public static void main(String[] args) 
 	{
-		int i,c;
-        for(i=2,c=1;c<=10001;i++)
-        {
-              int flag=0;
-        for(int j=2;j<i;j++)
-        {
-             if(i%j==0)
-             {
-                  flag=1;
-                  break;
-             }
-        }
-        if(flag==0)
-             c++;
-        }
-   System.out.println((i-1)+" is the 10001 st prime number.");
-
+		  int i,j,k=0,c=2;
+	     for(i=1;c<10001;i++)
+	     {
+	          if(isprime((6*i)-1)==0)
+	               {k=(6*i)-1;c++;}
+	          if(isprime((6*i)+1)==0&&c<10001)
+	               {k=(6*i)+1;c++;}
+	     }
+	     System.out.println("Result="+k);
 	}
-
+	 static int isprime(int a)
+	{
+	     for(int i=2;i<=Math.sqrt(a);i++)
+	     {
+	          if(a%i==0)
+	               return 1;
+	          else
+	               ;
+	     }
+	     return 0;
+	}
 }
